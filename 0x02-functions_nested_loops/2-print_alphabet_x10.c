3330a6b0123acfd3dd4b523b03b2d26927b9e5c1
Edward Agu Chukwuma <edwardinho6@gmail.com>
2022-10-18 16:06:50 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_10x - write alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_10x(void)
+{
+	char e;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (e = 'a'; e <= 'z'; e++)
+		{
+			_putchar(e);
+		}
+		_putchar('\n');
+	}
+}
