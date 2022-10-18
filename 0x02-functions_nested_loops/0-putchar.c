53032ff0b2a6a8f76d1683a0cf3e0e560cb5934a
Edward Agu Chukwuma <edwardinho6@gmail.com>
2022-10-18 04:55:41 -0700

    0 nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print _putchar without using printf
+ * Retirn: 0 (Success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
