08f057358cc53dd30bf40de39244cd585e1bdc42
Edward Agu Chukwuma <edwardinho6@gmail.com>
2022-10-18 15:19:12 -0700

    i alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * print_alphabet - print alphabets in lowercase followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e++)
+	{
+		_putchar(e);
+	}
+	_putchar('\n');
+}
