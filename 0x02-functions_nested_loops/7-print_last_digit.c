285428bba24d8438cc6178d5ed927128cc85570b
Edward Agu Chukwuma <edwardinho6@gmail.com>
2022-10-18 18:56:13 -0700

    7 last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,7 @@
  */
 int print_last_digit(int n)
 {
-	int last;
-
-	last = n % 10
-		;
-	if (last < 10)
-	{
-		last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+	while (n >= 10)
+		n /= 10;
+	return (n % 10);
 }
