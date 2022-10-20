7317ab30a182188f43d2c1ed24d1a7902f08b4eb
Edward Agu Chukwuma <edwardinho6@gmail.com>
2022-10-19 21:30:58 -0700

    my 0-man.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Testing function or positive or negative 
+ * Return: always  0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
