59e6fb1fc0e6af601d84245e3bad1afc621fdf36
Edward Agu Chukwuma <edwardinho6@gmail.com>
2022-10-20 04:38:39 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: always 0
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
