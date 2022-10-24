d81595742867ea1659503c59720151c4e440112d
Biggywalata <edwardinho6@gmail.com>
2022-10-24 15:43:01 -0700

    98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+void puts2(char *str);
+int _atoi(char *s);
+
+#endif
