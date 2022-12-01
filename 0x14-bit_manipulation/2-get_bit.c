1e2b0ac66a027439efca51c3e524979bdb12828b
Biggywalata <edwardinho6@gmail.com>
2022-12-01 14:19:53 -0800

    my commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
- *
- * Return: no return.
- */
-void print_binary(unsigned long int n)
+  * get_bit - Gets the value of a bit at a given index
+  * @n: The number to find the index
+  * @index: The index to find
+  *
+  * Return: ...
+  */
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (n >> 0)
+	unsigned int comp_i = 0;
+
+	while (n)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		if (comp_i == index)
+		{
+			if (n % 2)
+				return (1);
+			else
+				return (0);
+		}
+
+		n = n / 2;
+		comp_i++;
 	}
-	else
-	{
-		_putchar('0');
-	}
+
+	if (index > comp_i && index < 63)
+		return (0);
+
+	return (-)
 }
