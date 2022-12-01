13ec5635866477fc54c2c712cc1a0bd2c3a161d8
Biggywalata <edwardinho6@gmail.com>
2022-12-01 12:51:19 -0800

    my commits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -20,26 +20,3 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	return (nbits);
 }
-
-
-
-
-vi 100-get_endianness.c
-
-#include "main.h"
-
-/**
- * get_endianness - checks the endianness
- *
- * Return: 0 if big endian, 1 if little endian
- */
-int get_endianness(void)
-{
-	unsigned int x;
-	char *c;
-
-	x = 1;
-	c = (char *) &x;
-
-	return ((int)*c);
-}
